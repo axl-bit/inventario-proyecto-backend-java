7b10713801b2c512e864f058ed7b58bf0b3ca1bf
axl-bit <avillarmimbela@gmail.com>
2026-05-10 21:02:08 -0500

    add: configuración de authresponse, loginrequest y register request

---
+++ src/main/java/com/example/Auth/AuthResponse.java
@@ -0,0 +1,22 @@
+package com.example.Auth;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class AuthResponse {
+    private String token;
+    private String username;
+    private String email;
+    private String role;
+    private String message;
+    @Builder.Default
+    private boolean success = true;
+}
---
+++ src/main/java/com/example/Auth/RegisterRequest.java
@@ -0,0 +1,23 @@
+package com.example.Auth;
+
+
+import com.example.User.Role;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterRequest {
+    private String username;
+    private String password;
+    private String email;
+    private String firstName;
+    private String lastName;
+    @Builder.Default
+    private Role role = Role.USER;
+}
